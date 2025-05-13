fce1fdf205f08c3464f130510bad32c2aa10e6b5
root <root@compassionate-fermat.1cent.network>
2025-05-13 05:50:18 +0000

    Update documentation pages

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function About() {
+  return (
+    <Layout title="About">
+      <h1>About Us</h1>
+      <p>This is the about page for Crypp Docs.</p>
+    </Layout>
+  );
+}
